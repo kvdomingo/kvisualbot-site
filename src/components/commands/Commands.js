6e613f1672880257ac5f8536c222cf1f2ca7c052
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2021-01-04 01:27:56 +0800

    update docu for v1.0 of API

--- src/components/commands/Commands.js
+++ src/components/commands/Commands.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Badge, Card, CardHeader, CardBody, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
 import PropTypes from "prop-types";
 
-function Commands({ header, commands, newCommand }) {
+function Commands({ header, commands, labeledCommand, labelColor, labelName }) {
   return (
     <Card className="my-3" id={header.toLowerCase()}>
       <CardHeader>
-        {newCommand && <Badge color="danger">New!</Badge>} {header} commands
+        {labeledCommand && <Badge color={labelColor}>{labelName}</Badge>} {header} commands
       </CardHeader>
       <CardBody>
         <ListGroup>
-          {commands.data.map((command, i) => (
+          {commands.map((command, i) => (
             <ListGroupItem key={i}>
               <Row key={i}>
                 <Col sm="4">
@@ -42,6 +42,9 @@ Commands.propTypes = {
     optional: PropTypes.arrayOf(PropTypes.string),
     description: PropTypes.arrayOf(PropTypes.string.isRequired),
   }).isRequired,
+  labeledCommand: PropTypes.bool,
+  labelColor: PropTypes.string,
+  labelName: PropTypes.string,
 };
 
 export default Commands;
